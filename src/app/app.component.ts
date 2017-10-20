76491303b0dcc3d8b7f398f5542138ede4975ac8
Koichi <koichi.bt@gmail.com>
2017-10-20 09:07:23 -0300

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app';
